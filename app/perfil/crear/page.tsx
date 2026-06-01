508e670570b426f6a0569cc23b5e0bae38ed2d83
Laia Capellà Solé <laia.capella01@estudiant.upf.edu>
2026-06-01 11:58:37 +0200

    ara

--- app/perfil/crear/page.tsx
+++ app/perfil/crear/page.tsx
@@ -172,7 +172,7 @@ console.log('Error update avatar:', updateError)
       return
     }
 
-    router.push('/?refresh=' + Date.now())
+    window.location.href = '/'
   }
 
   if (cargandoPerfil) {
