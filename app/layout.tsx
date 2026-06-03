52ce15eea15d8e961ce81b7761509a8f2ce74dad
StudentPol <166695703+StudentPol@users.noreply.github.com>
2026-06-03 17:46:37 +0200

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
-import Chatbot from "@/components/Chatbot";
+import Chatbot from "../components/Chatbot";
 
 const playfair = Playfair_Display({
   variable: "--font-heading",
